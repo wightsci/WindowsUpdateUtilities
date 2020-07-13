8942b8cd45beb1e7820e76ef84eec08ca1bfb336
Stuart Squibb <stuart_squibb@hotmail.co.uk>
2020-07-13 20:27:50 +0100

    GWUL

--- WindowsUpdateUtilities/Public/Get-WindowsUpdateList.ps1
+++ WindowsUpdateUtilities/Public/Get-WindowsUpdateList.ps1
@@ -8,13 +8,14 @@ $SearchResult = $UpdateSearcher.Search("IsInstalled=0")
 
 if ($SearchResult.Updates.Count -eq 0) {
     Write-Output "There are no applicable updates."
-    Exit
 }
+else {
 
-Write-Output "List of applicable updates on the machine:"
+    Write-Output "List of applicable updates on the machine:"
 
-For ($I = 0; $I -le $SearchResult.Updates.Count-1; $I++) {
-    $Update = $SearchResult.Updates.Item($I)
-    Write-Output "$($I + 1)> $($Update.Title)"
+    For ($I = 0; $I -le $SearchResult.Updates.Count-1; $I++) {
+        $Update = $SearchResult.Updates.Item($I)
+        Write-Output "$($I + 1)> $($Update.Title)"
+    }
 }
 }
