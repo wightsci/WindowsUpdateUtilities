77b1b191f8ed2f0f561d1cc2f45cc9a5d6d8ecda
Stuart Squibb <stuart_squibb@hotmail.co.uk>
2020-07-13 20:10:45 +0100

    module

--- WindowsUpdateUtilities/WindowsUpdateUtilities.psm1
+++ WindowsUpdateUtilities/WindowsUpdateUtilities.psm1
@@ -145,5 +145,5 @@ $UpdateServiceManager = New-Object -ComObject "Microsoft.Update.ServiceManager"
 $ScriptRoot = Split-Path $Script:MyInvocation.MyCommand.Path
 $Public = @( Get-ChildItem -Path $ScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $ScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
-@($Public + $Private) | Foreach-Object { . $_.FullName }
+@($Public + $Private) | Foreach-Object { . $_.FullName ; Write-Verbose $_.FullName}
 Export-ModuleMember -Function $Public.Basename
